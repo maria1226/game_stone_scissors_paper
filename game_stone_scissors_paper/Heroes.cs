562b49e1129ab55aff3c051dfae40294e185431b
Maria Rakova <maria.c.rakova@gmail.com>
2020-04-15 10:54:24 +0300

    Add project files.

---
+++ game_stone_scissors_paper/Heroes.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace game_stone_scissors_paper
+{
+    class Heroes
+    {
+        //Fields
+        protected string name;
+        protected string weapon;
+        protected int numberOfLives;
+        protected int power;
+
+        public Heroes() { }//Default constructor
+
+        //Constructor
+        public Heroes(string name, string weapon, int numberOfLives, int power)
+        {
+            this.name = name;
+            this.weapon = weapon;
+            this.numberOfLives = numberOfLives;
+            this.power = power;
+        }
+
+        //Properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public string Weapon
+        {
+            get { return this.weapon; }
+            set { this.weapon = value; }
+        }
+
+        public int NumberOfLives
+        {
+            get { return this.numberOfLives; }
+            set { this.numberOfLives = value; }
+        }
+
+        public int Power
+        {
+            get { return this.power; }
+            set { this.power = value; }
+        }
+
+        //Method
+        //Print player status
+        public void PrintStats()
+        {
+            Console.WriteLine("Character's name is {0}", name);
+            Console.WriteLine("Number of lives: {0}", numberOfLives);
+            Console.WriteLine("Power value is: {0}", power);
+            Console.WriteLine("You play with: {0}", weapon);
+            Console.WriteLine("");
+        }
+
+
+
+    }
+
+    class Battle : Heroes//Inheritance-class Person
+    {
+        //Method
+        //Checking the user-specified value
+        public static void gameStart(Heroes a, Heroes b)
+
+        {
+            if (a.Weapon == "stone")
+            {
+                if (b.Weapon == "scissors")
+                {
+                    b.Power -= 1;
+                    b.NumberOfLives -= 1;
+                    Console.WriteLine($"{a.Name} is a winner");
+                }
+                else if (b.Weapon == "stone")
+                {
+                    Console.WriteLine("There is no winner");
+                }
+                else if (b.Weapon == "paper")
+                {
+                    a.Power -= 1;
+                    a.NumberOfLives -= 1;
+                    Console.WriteLine($"{b.Name} is a winner");
+                }
+
+            }
+            else if (a.Weapon == "scissors")
+            {
+                if (b.Weapon == "scissors")
+                {
+                    Console.WriteLine("There is no winner");
+                }
+
+                if (b.Weapon == "stone")
+                {
+                    a.Power -= 1;
+                    a.NumberOfLives -= 1;
+                    Console.WriteLine($"{b.Name} is a winner");
+                }
+
+                if (b.Weapon == "paper")
+                {
+                    b.Power -= 1;
+                    b.NumberOfLives -= 1;
+                    Console.WriteLine($"{a.Name} is a winner");
+                }
+
+            }
+            else if (a.Weapon == "paper")
+            {
+                if (b.Weapon == "scissors")
+                {
+                    a.Power -= 1;
+                    a.NumberOfLives -= 1;
+                    Console.WriteLine($"{b.Name} is a winner");
+                }
+
+                if (b.Weapon == "stone")
+                {
+                    b.Power -= 1;
+                    b.NumberOfLives -= 1;
+                    Console.WriteLine($"{a.Name} is a winner");
+                }
+                if (b.Weapon == "paper")
+                {
+
+                    Console.WriteLine("There is no winner");
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("There is no such weapon");
+
+            }
+
+
+        }
+
+    }
+}
---
+++ game_stone_scissors_paper/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace game_stone_scissors_paper
+{
+    //Да се напрви клас Герои. 
+    //Да се направят поне 2 героя.
+    //Всеки герои има име, оръжие,брой животи и сила. 
+    //Да се направи клас Битка и да се организира съревнование между 2 героя. 
+    //Да се отпечата победителя. При един удар се губи живот.
+    //Животите са 2. Силата е от 1 до 3.  Оръжия: камък, ножица, хартия.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select your weapon: stone,scissors or paper");
+            string weapon = Console.ReadLine();//User input
+
+            Heroes h1 = new Heroes("Mario Bros", weapon, 2, 3);//Make object from class Heroes
+
+            h1.PrintStats();//Calling the method from class Heroes to print object data
+
+            Console.WriteLine("Select a weapon for the second player: stone,scissors or paper");
+
+            string weapon1 = Console.ReadLine();//User input
+
+            Heroes h2 = new Heroes("Scorpion", weapon1, 2, 3);//Make the second object from class Heroes
+
+            h2.PrintStats();//Calling the method from class Heroes to print object data
+
+            Console.WriteLine("");
+            Console.WriteLine("Start the game");
+            Console.ReadKey();
+
+            Console.Clear();
+            Battle.gameStart(h1, h2);//Calling method from class Battle to start game
+
+            Console.WriteLine("Do you want to see the status of your characters? yes or no.");
+            string check = Console.ReadLine();//User input
+            //check
+            if (check == "yes")
+            {
+                h1.PrintStats();
+                h2.PrintStats();
+            }
+            else if (check == "no")
+            {
+                Console.Clear();
+            }
+
+        }
+    }
+}
